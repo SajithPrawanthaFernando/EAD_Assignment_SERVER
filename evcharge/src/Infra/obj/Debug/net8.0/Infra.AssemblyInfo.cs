b4844ab8a075cc8563adb1112e018ffafb3e8013
sajithfdo <sajith@teachmeit.lk>
2025-10-03 21:45:31 +0530

    Dll files changes

--- evcharge/src/Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
+++ evcharge/src/Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1b5d181cc9b10b29e12b8f309bbf1cd15cf787b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d9a8c48b8c21616e81c3ad2e2878484c97823a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
